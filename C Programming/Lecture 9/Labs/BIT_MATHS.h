f9c3d40aaf9172f8ccd83e76a87592f88fb2f51a
Omar Tarek <omarmoharam193@gmail.com>
2024-02-12 16:33:25 +0200

    std types & bit maths headers

--- C Programming/Lecture 9/Labs/BIT_MATHS.h
+++ C Programming/Lecture 9/Labs/BIT_MATHS.h
@@ -1,4 +1,9 @@
+#ifndef BIT_MATHS_H
+#define BIT_MATHS_H
+
 #define SET_BIT(VAR, BIT_NO) VAR |= (1<<BIT_NO)
 #define CLR_BIT(VAR, BIT_NO) VAR &= (~(1<<BIT_NO))
 #define TOGGLE_BIT(VAR, BIT_NO) VAR ^= (1<<BIT_NO)
 #define GET_BTI(VAR, BIT_NO) VAR = ((VAR>>BIT_NO) & 1)
+
+#endif
