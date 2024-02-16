6557a123821a12a741359132eae455b31a1f0486
Omar Tarek <omarmoharam193@gmail.com>
2024-02-16 15:38:41 +0200

    linked list deletedNode

--- C Programming/Lecture 10/Labs/single_linkedlist.c
+++ C Programming/Lecture 10/Labs/single_linkedlist.c
@@ -13,6 +13,8 @@ typedef struct Node
 // static is here to protect the head pointer from being used in another file.c
 static Node* head = NULL;
 static Node* current = NULL;
+static Node* deletedNode = NULL;
+short char flag = 0;
 
 void insertAtFirst(int value) {
     //creating a node
@@ -56,6 +58,53 @@ void insertAtLeast(int value) {
 
 }
 void deleteValue(int value) {
+    Node *prev = NULL;
+    // if list is empty
+    if (head == NULL)
+    {
+        printf("The list is empty");
+    }
+    // if value is in the first node
+    else if (head->data == value)
+    {
+        deletedNode = head;
+        head = deletedNode->next;
+        free(deletedNode);
+        flag = 1;
+    }
+    // if value is not in the first node
+    else
+    {
+        current = head->next;
+        prev = head;
+        while (current != NULL)
+        {
+            if (current->data == value)
+            {
+                deletedNode = current
+                current = NULL;
+                flag = 1;
+                prev->next = deletedNode->next;
+                free(deletedNode);
+                break;
+            }
+            
+            prev = current;
+            current = current->next;
+        }
+        
+        if (flag == 0) // indication if the value is not found
+        {
+            printf("The value is not found");
+        }
+        else
+        {
+            flag = 0;
+        }
+        
+    }
+    
+    
 
 }
 int searchValue(int value) {
